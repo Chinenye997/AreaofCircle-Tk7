9487149b1783f0534b4a2849dea92fdab18808ab
chinenye997 <edehg997@gmail.com>
2024-08-27 01:26:15 +0100

    Areaofcircle task7

---
+++ DesktopApps for AreaofCircle/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace DesktopApps_for_AreaofCircle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtRadius = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.IbIAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(179, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Area of circle";
+            // 
+            // TxtRadius
+            // 
+            this.TxtRadius.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtRadius.Location = new System.Drawing.Point(168, 162);
+            this.TxtRadius.Multiline = true;
+            this.TxtRadius.Name = "TxtRadius";
+            this.TxtRadius.Size = new System.Drawing.Size(205, 60);
+            this.TxtRadius.TabIndex = 1;
+            this.TxtRadius.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(168, 293);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(205, 84);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // IbIAnswer
+            // 
+            this.IbIAnswer.AutoSize = true;
+            this.IbIAnswer.BackColor = System.Drawing.SystemColors.Desktop;
+            this.IbIAnswer.Location = new System.Drawing.Point(551, 162);
+            this.IbIAnswer.Name = "IbIAnswer";
+            this.IbIAnswer.Size = new System.Drawing.Size(0, 46);
+            this.IbIAnswer.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(24F, 46F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 564);
+            this.Controls.Add(this.IbIAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.TxtRadius);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "CALCULATION OF CIRCLE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtRadius;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label IbIAnswer;
+    }
+}
+
---
+++ DesktopApps for AreaofCircle/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApps_for_AreaofCircle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // ' Delcare veriables
+            double radius, area;
+            radius = Convert.ToDouble(TxtRadius.Text);
+
+            area = Math.PI * radius * radius;
+
+
+            IbIAnswer.Text = area.ToString();
+            //IbIAnswer.BackColor = Color.White;
+
+
+            //'clear text box
+            TxtRadius.Clear();
+
+            //'set foucs after clearing 
+            TxtRadius.Focus();
+           
+        }
+    }
+}
